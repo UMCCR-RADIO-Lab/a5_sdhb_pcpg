b72203300598d770d8cb09ab6c4152fde74503cb
Aidan Flynn <aidan.flynn@unimelb.edu.au>
2023-06-29 13:47:52 +1000

    Added E169 to MGMT plotting code

--- tertiary/scripts/figures/A5_FigX_MGMT_meth_expr.r
+++ tertiary/scripts/figures/A5_FigX_MGMT_meth_expr.r
@@ -1,7 +1,8 @@
 ###########################################
 ###########################################
 ##                                       ##
-##   E167 Treatment signature / MGMT     ##
+##   E167/E169 MGMT                      ##
+##   Methylation/Expression              ##
 ##                                       ##
 ##   Script for producing a scatter      ##
 ##   plots showing MGMT methylation      ##
@@ -49,37 +50,60 @@ probe_beta <- b_vals %>%
   filter(Probe %in% MGMT_promoter_probes) %>% 
   pivot_longer(cols=-Probe, 
                names_to = "A5_ID", 
-               values_to = "Beta") %>% 
-  mutate(point_color=ifelse(grepl("167", A5_ID), "Red", "Black"), 
-         Label=ifelse(grepl("167", A5_ID), A5_ID, NA))
+               values_to = "Beta") 
 
 mgmt_expr <- cpm(a5_wts_dge_list$SDHB, log = T) %>% 
   as_tibble(rownames = "ensgid_symbol") %>% 
   filter(ensgid_symbol == "ENSG00000170430.10_MGMT") %>% 
   pivot_longer(cols=-ensgid_symbol, 
                names_to = "A5_ID", 
-               values_to = "log2cpm") %>% 
-  mutate(point_color=ifelse(grepl("167", A5_ID), "Red", "Black"), 
-         Label=ifelse(grepl("167", A5_ID), A5_ID, NA))
+               values_to = "log2cpm")
 
-pj <- position_jitter(width=0.2, seed=100)
-gg_meth <- ggplot(probe_beta, 
-       aes(x=Probe, y=Beta, color=point_color, label=Label)) + 
-  geom_point(position = pj) + 
-  geom_text(mapping=aes(y=Beta+0.02),position = pj, color="black") +
-  scale_color_identity() + 
-  theme_bw() +
-  xlab("Probe") +
-  ylab("Beta")
+mgmt_plots <- list()
+for (patient in c("E167", "E169"))
+{
+  
+  probe_beta <- probe_beta %>% 
+  mutate(point_color=ifelse(grepl(patient, A5_ID), "Red", "Black"), 
+         Label=ifelse(test = grepl(patient, A5_ID), 
+                      yes = dplyr::recode(A5_ID, !!!setNames(a5_anno$PublicationID, a5_anno$A5_ID)), 
+                      no = NA))
 
-gg_expr <- ggplot(mgmt_expr, 
-       aes(x="MGMT", y=log2cpm, color=point_color, label=Label)) + 
-  geom_point(position = pj) + 
-  geom_text_repel(mapping=aes(y=log2cpm),position = pj, color="black") +
-  scale_color_identity() + 
-  theme_bw() +
-  xlab("") +
-  ylab("CPM (log2)")
+  mgmt_expr <- mgmt_expr %>% 
+  mutate(point_color=ifelse(grepl(patient, A5_ID), "Red", "Black"), 
+         Label=ifelse(test = grepl(patient, A5_ID), 
+                      yes = dplyr::recode(A5_ID, !!!setNames(a5_anno$PublicationID, a5_anno$A5_ID)), 
+                      no = NA))
 
-gg_meth + gg_expr + plot_layout(nrow=1, widths = c(2,1))
+  pj <- position_jitter(width=0.2, seed=100)
+  gg_meth <-
+    ggplot(probe_beta, 
+           aes(x=Probe, y=Beta, 
+               color=point_color, 
+               label=Label)) + 
+    geom_point(position = pj) + 
+    geom_text(mapping=aes(y=Beta+0.02),position = pj, color="black") +
+    scale_color_identity() + 
+    theme_bw() +
+    xlab("Probe") +
+    ylab("Beta")
+  
+  gg_expr <- 
+    ggplot(mgmt_expr, 
+         aes(x="MGMT", y=log2cpm, color=point_color, label=Label)) + 
+    geom_point(position = pj) + 
+    geom_text_repel(mapping=aes(y=log2cpm),position = pj, color="black") +
+    scale_color_identity() + 
+    theme_bw() +
+    xlab("") +
+    ylab("CPM (log2)")
+  
+  mgmt_plots[[patient]] <- 
+    gg_meth + 
+    gg_expr + 
+    plot_layout(nrow=1, widths = c(2,1))
+
+  ggsave(filename = paste0("./a5/tertiary/results/plots/methylation_vs_expression/",patient,"_MGMT_expr_meth.pdf"),
+        plot = mgmt_plots[[patient]],device = pdf(), width = 5, height = 9,units = "in")
+}
 
