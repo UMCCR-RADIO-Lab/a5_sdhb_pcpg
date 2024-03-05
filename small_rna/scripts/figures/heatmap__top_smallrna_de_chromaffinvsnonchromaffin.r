9cf83225aa72a27844a3bb513f69ce4b1a69f232
Aidan Flynn <aidan.flynn@unimelb.edu.au>
2024-03-05 17:15:23 +1100

    modifications to chromaffin/non-chromaffin heatmaps

--- small_rna/scripts/figures/heatmap__top_smallrna_de_chromaffinvsnonchromaffin.r
+++ small_rna/scripts/figures/heatmap__top_smallrna_de_chromaffinvsnonchromaffin.r
@@ -18,95 +18,9 @@ source("/g/data/pq08/projects/ppgl/a5/methylation/scripts/helpers/plot_methylati
 #Small RNA
 source("./a5/small_rna/scripts/differential_expression/a5_smallrna_differential_expression.r")
 
-#################
-# Heatmap - WTS #
-#################
-
-sample.order <- a5_anno %>% 
-  filter(A5_ID %in% colnames(a5_wts_lcpm_list[["SDHB"]])) %>% 
-  arrange(cell_of_origin, differential_group_anatomy) %>% 
-  pull(A5_ID)
-
-contrast_membership <- contrastdesign_to_memberlist("Non_chromaffin_vs_Chromaffin", contrast_matrix_hn, design_matrix_hn, contrast_name_sep="_vs_")
-
-plot.data <- GOI.plot %>% 
-  inner_join(a5_wts_lcpm_list[["SDHB"]][GOI.plot$Gene,sample.order] %>% 
-               data.frame(check.names = F) %>%  
-               tibble::rownames_to_column("Gene")) %>% inner_join(ensid_to_biotype) %>% 
-  data.frame(check.names = F) %>% 
-  distinct()
-
-
-plot.data.EnsIds <- gsub("(ENSG[0-9]+)([.][0-9]+)?_.+","\\1", plot.data$Gene)
-data_loader_ensgid_to_chr(plot.data.EnsIds, use_cache=TRUE)
-ensgid_to_chr <- data.frame(Gene=plot.data$Gene, ensembl_gene_id=plot.data.EnsIds) %>% left_join(ensgid_to_chr) %>% dplyr::select(-ensembl_gene_id, -start_position, -end_position)
-plot.data <- plot.data %>% left_join(ensgid_to_chr)
-plot.data$chromosome_name <- paste0("chr", plot.data$chromosome_name)
-
-rownames(plot.data) <- plot.data$Symbol_Label
-
-hm.annot.data.col <- a5_anno %>% 
-  filter(A5_ID %in% sample.order) %>% 
-  mutate(A5_ID=factor(A5_ID, levels=sample.order)) %>% 
-  arrange(A5_ID) %>% 
-  dplyr::select(A5_ID, differential_group_anatomy)
-
-
-hm.annot.col = HeatmapAnnotation(
-  df = hm.annot.data.col[, -1],
-  which = "column",
-  show_legend = TRUE,
-  col = list("differential_group_anatomy" = location_cols))
-
-
-chrom_cols<-c(RColorBrewer::brewer.pal(n=8, name="Set1"),
-              RColorBrewer::brewer.pal(n=8, name="Set2"),
-              RColorBrewer::brewer.pal(n=length(unique(plot.data$chromosome_name))-16, name="Set3"))[1:length(unique(plot.data$chromosome_name))]
-
-hm.annot.row = HeatmapAnnotation(df = plot.data[,c( "In_DMR", "chromosome_name"), drop=F], #"source",
-                                 which="row", 
-                                 show_legend = TRUE,
-                                 col=list(
-                                   #"source"=setNames(RColorBrewer::brewer.pal(n=length(unique(plot.data$source)), name="Set3")[1:length(unique(plot.data$source))], unique(plot.data$source)),
-                                   In_DMR=c("Yes"="Black", "No"="lightgrey"),
-                                   gene_biotype=bio_type_cols,
-                                   "chromosome_name"=setNames(
-                                     chrom_cols,
-                                     unique(plot.data$chromosome_name)) 
-                                 ))
-
-
-
-hm_bulk_gex <- Heatmap(t(scale(t(plot.data %>% dplyr::select(-Gene, -Symbol, -gene_biotype, -In_DMR, -Symbol_Label, -chromosome_name)))),
-                       #col = col_fun,
-                       #row_split = plot.data$source,
-                       column_split = hm.annot.data.col$differential_group_anatomy,
-                       #width = ncol(bulk_heatmap_deg)*unit(2, "mm"),
-                       #height = ncol(bulk_heatmap_deg)*unit(4, "mm"),
-                       row_gap = unit(2, "mm"),
-                       column_gap = unit(2, "mm"),
-                       row_title_rot = 90,
-                       top_annotation = hm.annot.col,
-                       left_annotation = hm.annot.row,
-                       column_title_rot = 90,
-                       cluster_columns = FALSE,
-                       #show_column_dend = FALSE,
-                       cluster_column_slices = FALSE,
-                       cluster_rows = TRUE,
-                       #show_row_dend = FALSE,
-                       cluster_row_slices = FALSE,
-                       #row_labels = plot.data$Symbol_Label,
-                       show_row_names = TRUE,
-                       row_names_side = "left",
-                       show_column_names  = TRUE,
-                       row_names_gp = gpar(fontsize = 7, fontface = "italic"),
-                       #heatmap_legend_param  = hm_legend_params
-)
-
-
-#######################
-# Heatmap Genes - WTS #
-#######################
+#############################
+# Heatmap Genes - Small RNA #
+#############################
 
 genes_per_group <- 100
 gene_select <- "topbottom_logfc" #"top_abslogfc" "topbottom_logfc" "pval_logfc_direction"
@@ -156,10 +70,12 @@ GOI_plot <- smallrna_top_tables[["Non_chromaffin_vs_Chromaffin"]][["Non_chromaff
 #######################
 
 a5_anno_use <- a5_anno %>% 
-  mutate(differential_group_anatomy = case_match(A5_ID, 
-                                                 "E185-1"~"Head_neck", 
-                                                 "E128-1"~"Abdominal_Thoracic",
-                                                 .default = differential_group_anatomy)) %>% 
+  mutate(primary_location_plotting = case_when(
+    A5_ID == "E185-1" ~ "Head and neck", 
+    A5_ID == "E128-1" ~ "Extraadrenal (abdominal/thoracic)",
+    primary_location_plotting == "Extraadrenal (bladder)" ~ "Extraadrenal (abdominal/thoracic)",
+    differential_group_anatomy == "Thoracic_non_chromaffin" ~ "Thoracic (non-chromaffin)", 
+    .default = primary_location_plotting)) %>%
   filter(A5_ID %in% colnames(a5_smallrna_lcpm_list[["SDHB"]])) %>% 
   arrange(differential_group_anatomy, cell_of_origin) %>% 
   mutate(A5_ID = factor(A5_ID, levels=.$A5_ID))
