d643eac6689bda9445ca6cd4c1fdb328fc9e5bc1
Aidan Flynn <aidan.flynn@unimelb.edu.au>
2024-04-10 14:52:14 +1000

    update contrasts used in biotype plot

--- wts/scripts/figures/de_contrast_by_gene_biotype.r
+++ wts/scripts/figures/de_contrast_by_gene_biotype.r
@@ -1,3 +1,4 @@
+setwd("/g/data/pq08/projects/ppgl/")
 source("./a5/wts/scripts/differential_expression/a5_wts_differential_expression.r")
 source("./a5/sample_annotation/scripts/data_loaders/a5_color_scheme.r")
 
@@ -13,15 +14,15 @@ GOI.met <-
   mutate(source ="Metastatic_All_vs_NonMetPri_WT")
 
 
-GOI.tert <- wts_top_tables[["genosampletype"]][["TERT_All_vs_NonTERT"]] %>% 
+GOI.tert <- wts_top_tables[["genosampletype"]][["TERT_PriMet_vs_NonMetPri_WT"]] %>% 
   filter(adj.P.Val < adj_pval_cutoff) %>% 
-  mutate(source="TERT_All_vs_NonTERT")
+  mutate(source="TERT_PriMet_vs_NonMetPri_WT")
 
 
 GOI.atrx <-
-  wts_top_tables[["genosampletype"]][["ATRX_All_vs_NonATRX"]] %>% 
+  wts_top_tables[["genosampletype"]][["ATRX_PriMet_vs_NonMetPri_WT"]] %>% 
   filter(adj.P.Val < adj_pval_cutoff) %>%  
-  mutate(source="ATRX_All_vs_NonATRX")
+  mutate(source="ATRX_PriMet_vs_NonMetPri_WT")
 
 
 GOI <- bind_rows(GOI.met, GOI.tert, GOI.atrx)
@@ -45,17 +46,13 @@ plot_data <- GOI %>%
 plot_data <- plot_data %>% 
   mutate(source = factor(as.character(source), 
                          levels = c("Metastatic_All_vs_NonMetPri_WT", 
-                                    "TERT_All_vs_NonTERT", "ATRX_All_vs_NonATRX")),
-         source=forcats::fct_recode(source, 
-                                    "All Metastasis vs NMP"="Metastatic_All_vs_NonMetPri_WT", 
-                                    "TERT vs Rest"="TERT_All_vs_NonTERT", 
-                                    "ATRX vs Rest"="ATRX_All_vs_NonATRX")) 
+                                    "TERT_PriMet_vs_NonMetPri_WT", "ATRX_PriMet_vs_NonMetPri_WT")))
 
 plot_data <- plot_data 
 
-fill_pal <- c("All Metastasis vs NMP"=ColorPalette[["DarkRed2"]],
-              "TERT vs Rest"=driver_cols[["TERT"]],
-              "ATRX vs Rest"=driver_cols[["ATRX"]])
+fill_pal <- c("Metastatic_All_vs_NonMetPri_WT"=ColorPalette[["DarkRed2"]],
+              "TERT_PriMet_vs_NonMetPri_WT"=driver_cols[["TERT"]],
+              "ATRX_PriMet_vs_NonMetPri_WT"=driver_cols[["ATRX"]])
 
 col_pal <- c("Up-regulated"="red",
               "Down-regulated"="blue")
